ac5360a2367df0267bfe27c02d26b0450017dd99
Harris Pervez <harrykhan0910@gmail.com>
2025-08-09 19:28:02 +0500

    Time: 51 ms (7.49%) | Memory: 12.7 MB (24.42%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -5,10 +5,5 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        L=nums1+nums2
-        new=[]
-        for i in L:
-            if i in nums1 and i in nums2 and i not in new:
-                new.append(i)
-        return new
+        return list(set([i for i in nums1+nums2 if i in nums1 and i in nums2]))
         
